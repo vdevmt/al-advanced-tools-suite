a91302ffbf762a9f83f96728c3c3c3c6046a521e
vdevmt <vdevmt@outlook.it>
2025-01-02 19:20:13 +0100

    Object namespace in statusbar item

--- src/fileMgt/alObjectInfoStatusBar.ts
+++ src/fileMgt/alObjectInfoStatusBar.ts
@@ -13,7 +13,7 @@ export function createObjectInfoStatusBarItem(): vscode.StatusBarItem {
 
         const objectInfoStatusBarItem = vscode.window.createStatusBarItem(alignment);
         objectInfoStatusBarItem.text = `$(info)`;
-        objectInfoStatusBarItem.tooltip = makeTooltip('', '');
+        objectInfoStatusBarItem.tooltip = makeTooltip(null, '');
         objectInfoStatusBarItem.command = {
             command: 'ats.showOpenALObjects',
             title: `ATS: Show open AL Objects`
@@ -30,14 +30,14 @@ export function createObjectInfoStatusBarItem(): vscode.StatusBarItem {
 
 export async function updateObjectInfoStatusBarByDocument(objectInfoStatusBarItem: vscode.StatusBarItem, document: vscode.TextDocument) {
     objectInfoStatusBarItem.text = '';
-    objectInfoStatusBarItem.tooltip = makeTooltip('', '');
+    objectInfoStatusBarItem.tooltip = makeTooltip(null, '');
 
     if (document) {
         if (alFileMgr.isALObjectDocument(document)) {
             let alObject: ALObject;
             alObject = new ALObject(document.getText(), document.fileName);
             let objectInfoText = alFileMgr.makeALObjectDescriptionText(alObject);
-            objectInfoStatusBarItem.tooltip = makeTooltip(objectInfoText, alObject.extendedObjectName);
+            objectInfoStatusBarItem.tooltip = makeTooltip(alObject, objectInfoText);
             objectInfoStatusBarItem.text = `$(info) ${objectInfoText}`;
         }
     }
@@ -52,14 +52,25 @@ export async function updateObjectInfoStatusBar(objectInfoStatusBarItem: vscode.
     }
 }
 
-function makeTooltip(objectInfoText: string, extendedObjectName: string): vscode.MarkdownString {
+function makeTooltip(alObject: ALObject, objectInfoText: string): vscode.MarkdownString {
     const markdownTooltip = new vscode.MarkdownString();
     markdownTooltip.appendMarkdown("### **AL Object Info (ATS)**\n\n");
     if (objectInfoText) {
         markdownTooltip.appendMarkdown(`${objectInfoText}\n\n`);
+    }
 
-        if (extendedObjectName) {
-            markdownTooltip.appendMarkdown(`extends ${alFileMgr.addQuotesIfNeeded(extendedObjectName)}\n\n`);
+    if (alObject) {
+        if (alObject.objectNamespace) {
+            markdownTooltip.appendMarkdown(`${alObject.objectNamespace}\n\n`);
+        }
+
+        if (alObject.extendedObjectName) {
+            markdownTooltip.appendMarkdown(`extends ${alFileMgr.addQuotesIfNeeded(alObject.extendedObjectName)}\n\n`);
+        }
+
+        if (alObject.objectFileName) {
+            let filePath = vscode.workspace.asRelativePath(alObject.objectFileName);
+            markdownTooltip.appendMarkdown(`${filePath}`);
         }
     }
 
